9f9f78739237437e6197cdf6bc9614f5f7ac54cc
Ryan Roemer <ryan@loose-bits.com>
2014-11-06 16:25:49 -0800

    Update script.

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -6,16 +6,35 @@ var DB_PATH = __dirname + "/../server/notes.sqlite";
 var OPEN_STATE = sql.OPEN_READWRITE | sql.OPEN_CREATE;
 /*jslint bitwise: false */
 var TABLE_NAME = "notes";
+var ROWS = "(id integer primary key autoincrement, title text, text text)";
+var CB = function (err) { if (err) { throw err; } };
 
-var db = new sql.Database(DB_PATH, OPEN_STATE, function (err) {
-  if (err) {
-    db.close();
-    throw err;
-  }
+var init = module.exports = function (dbPath, callback) {
+  dbPath = typeof dbPath !== "undefined" ? dbPath : DB_PATH;
+  callback = callback || CB;
 
-  db.run("drop table if exists " + TABLE_NAME, function () {
-    db.run("create table " + TABLE_NAME +
-      " (id integer primary key autoincrement, title text, text text)",
-      function () { db.close(); });
+  var db = new sql.Database(dbPath, OPEN_STATE, function (err) {
+    if (err) {
+      db.close();
+      return callback(err);
+    }
+
+    db.run("drop table if exists " + TABLE_NAME, function (err) {
+      if (err) { return callback(err); }
+      db.run("create table " + TABLE_NAME + " " + ROWS, function (err) {
+        db.close();
+        callback(err);
+      });
+    });
   });
-});
+};
+
+// Script. Use defaults (init dev. database).
+if (require.main === module) {
+  init(DB_PATH, function (err) {
+    if (err) {
+      console.log("ERROR: ", err);
+    }
+    console.log("Finished initialization of: " + DB_PATH);
+  });
+}
